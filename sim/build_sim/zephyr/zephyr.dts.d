f0bf65de4896cad44eeda448a5056fb2f7293e7a
Poghosyan-Davit003 <davitpoghosyan@gbutech.com>
2025-07-09 13:23:09 +0400

    first commit

---
+++ sim/build_sim/zephyr/zephyr.dts.d
@@ -0,0 +1,25 @@
+empty_file.o: /home/davit/Desktop/sim/zephyr/misc/empty_file.c \
+ /home/davit/Desktop/sim/sim-fw/boards/sim-cxx/sim-cxx.dts \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch582f.dtsi \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch58x-common.dtsi \
+ /home/davit/Desktop/sim/zephyr/dts/common/mem.h \
+ /home/davit/Desktop/sim/zephyr/dts/common/freq.h \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/qingke-v4.dtsi \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch58xf-common.dtsi \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch582x-common.dtsi \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch582f-pinctrl.dtsi \
+ /home/davit/Desktop/sim/zephyr/dts/riscv/wch/ch58x/ch58x-common-pinctrl.dtsi \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/dt-bindings/pinctrl/ch58x-pinctrl.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_macro.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_internal.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_loops.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_listify.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_internal_is_eq.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_internal_util_inc.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_internal_util_dec.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/sys/util_internal_util_x2.h \
+ /home/davit/Desktop/sim/zephyr/include/zephyr/dt-bindings/pinctrl/ch5xx-pinctrl-common.h \
+ /home/davit/Desktop/sim/sim-fw/apps/cartridge/sim-cxx.overlay
