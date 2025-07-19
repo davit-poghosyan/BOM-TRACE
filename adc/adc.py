c2de8d4c008a77c4fb36f6c02abd658c0ddc7e28
Poghosyan-Davit003 <davitpoghosyan@gbutech.com>
2025-07-19 20:46:18 +0400

    adc modificaion

--- adc/adc.py
+++ adc/adc.py
@@ -6,11 +6,11 @@ ADC_CHANNEL2 = 2       # ADC2
 VREF = 1.8             # Reference voltage (Orange Pi CM4 uses 1.8V)
 ADC_RESOLUTION = 1023  # 10-bit ADC (0-1023)
 
-delay_ms = 1000
-delay_sec = 1/1000
+adc_read_delay_ms = 1/1000
+prog_delay = 1000/1000
 
-buff = []
 
+# buff = 0 
 
 def read_adc(channel):
         path = f"/sys/bus/iio/devices/iio:device0/in_voltage{channel}_raw"
@@ -18,32 +18,44 @@ def read_adc(channel):
             value = int(f.read().strip())
         return value
 
-def volt_to_amp(volt):
-    return volt / (0.015 * 20)
-
 def raw_adc_val_to_volt(adc_val):
     return adc_val/ ADC_RESOLUTION * VREF
 
-def adc_read(buff):
+def volt_to_amp(volt):
+    return volt / (0.015 * 20)
+
+def amp_calculatoin(channel):
+
     try:
+        buff = 0
         for i in range(20):
-            adc_raw3 = read_adc(ADC_CHANNEL3)
-            adc_raw2 = read_adc(ADC_CHANNEL2)
+            # adc_raw3 = read_adc(ADC_CHANNEL3)
            # voltage3 = (adc_raw3 / ADC_RESOLUTION) * VREF
             #voltage2 = (adc_raw2 / ADC_RESOLUTION) * VREF
 
-            voltage3 = raw_adc_val_to_volt(adc_raw3)
-            voltage2 = raw_adc_val_to_volt(adc_raw2)
-            
-            amp2 = volt_to_amp(volt_to_amp(voltage2))
-
-            #print(f"ADC: {voltage}")
-            print(f"voltage(ADC3): {voltage3 *3}")
+            # voltage3 = raw_adc_val_to_volt(adc_raw3)
+            adc_raw = read_adc(channel)
+            voltage = raw_adc_val_to_volt(adc_raw) 
+            amp = volt_to_amp(voltage)
+            buff += amp
+       
+            #print(f"ADC: {voltagde}")
+            # print(f"voltage(ADC3): {voltage3 *3}")
             # print(f"amper(ADC2) {voltage2 / (0.015 * 20)}")
-            print(f"amper(ADC2) {amp2}")
-            print(f"\n")
-            time.sleep(delay_sec)
+            # print(f"amper(ADC2) {amp2}")
+            # print(f"\n")
+            time.sleep(adc_read_delay_ms)
 
+        return buff/20
+    
     except KeyboardInterrupt:
             print("\nExit program.") 
 
+
+try:
+    while(True):
+        amp2 = amp_calculatoin(ADC_CHANNEL2)
+        print(f"amper(adc2) {amp2}")
+        time.sleep(prog_delay)
+except KeyboardInterrupt:
+    print("\nExit program.") 
